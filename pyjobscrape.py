e48c69dfe3f1da7091343b27691269893c5944a1
davecthomas <davidcthomas@gmail.com>
2022-04-09 16:25:33 -0700

    Saving results to a dataframe for analysis and a csv.

--- pyjobscrape.py
+++ pyjobscrape.py
@@ -1,6 +1,8 @@
 from urllib.request import Request, urlopen
 import urllib.parse
 from bs4 import BeautifulSoup as soup
+import re
+import pandas as pd
 
 from datetime import datetime
 import webbrowser
@@ -31,8 +33,22 @@ def get_job(job_page, job_id):
     req = Request(job_url, headers={'User-Agent': 'Mozilla/5.0'})
     webpage = urlopen(req).read()
     pagesoup = soup(webpage, "html.parser")
-    job_dict["h1"] = pagesoup.find('h1').text
-    print(job_dict)
+    job_dict["job_title"] = pagesoup.find('h1').text
+    job_dict["company"] = pagesoup.find('a').text
+    job_type_element = pagesoup.find("div", string="Full-time")
+    if job_type_element is not None:
+        job_dict["job_type_full_time"] = True
+    job_type_element = pagesoup.find("div", string="Part-time")
+    if job_type_element is not None:
+        job_dict["job_type_part_time"] = True
+    company_rating_element = pagesoup.find('div', attrs={"class": "icl-Ratings-starsCountWrapper"})
+    if company_rating_element is not None and company_rating_element.has_attr("aria-label"):
+        job_dict["company_rating_fulltext"] = company_rating_element["aria-label"]
+        list_rating_info = re.findall(r"[-+]?(?:\d*\.\d+|\d+)",job_dict["company_rating_fulltext"])
+        if len(list_rating_info) == 3:
+            job_dict["company_rating"] = list_rating_info[0]
+            job_dict["company_rating_max_potential"] = list_rating_info[1]
+            job_dict["company_rating_num_employee_votes"] = list_rating_info[2]
 
     # list_urls = []
     # for a in a_s:
@@ -56,11 +72,11 @@ list_dict_config_job_sites = [
     {
         "job_site": "Indeed.com",
         "url": "https://www.indeed.com/jobs?as_and&as_phr={}&as_any&as_not&as_ttl&as_cmp&jt=parttime&st&sr=directhire&salary&radius=25&l&fromage=any&limit={}&sort&psf=advsrch&from=advancedsearch&vjk=19ff24d735a88a04",
-        "list_length": 3, # Can be up to 50 for Indeed. Keep it small for testing
+        "list_length": 2, # Can be up to 50 for Indeed. Keep it small for testing
         "sleep_time_between_requests": 2, # seconds to sleep to prevent site from knowing you're a scraper
         "random_sleep_variation": 1, # add on to sleep to look more human
         "job_title": "nursing aide",
-        "job_page": "https://www.indeed.com/viewjob?jk="
+        "job_page": "https://www.indeed.com/viewjob?jk=",
     }
 ]
 
@@ -68,14 +84,15 @@ list_dict_config_job_sites = [
 import time
 ssl._create_default_https_context = ssl._create_unverified_context
 print("\nScraping\n")
-list_job_dict = []
+list_jobs_dict = []
 
 for idx, config_job_site_dict in enumerate(list_dict_config_job_sites):
     url = config_job_site_dict["url"].format(   # Format the URL to include the job title and results length
         urllib.parse.quote(config_job_site_dict["job_title"], safe=""),
         config_job_site_dict["list_length"])
     print(f"getting {url}")
-    print(f'Looking for {config_job_site_dict["job_title"]} at {config_job_site_dict["job_site"]} on {datetime.now().strftime("%A %B %d %H:%M:%S")}')
+    datetime_string = datetime.now().strftime("%A %B %d %H:%M:%S")
+    print(f'Looking for {config_job_site_dict["job_title"]} at {config_job_site_dict["job_site"]} on {datetime_string}')
     list_job_ids = get_jobs_IDs(url)
     print(f'Opening up {len(list_job_ids)} job pages on {config_job_site_dict["job_site"]}')
     for job_id in list_job_ids:
@@ -83,4 +100,9 @@ for idx, config_job_site_dict in enumerate(list_dict_config_job_sites):
         sleep_per_iteration_rand = config_job_site_dict["sleep_time_between_requests"] + round(random.random()*config_job_site_dict["random_sleep_variation"], 1)
         print(f"Sleeping for {sleep_per_iteration_rand}...")
         time.sleep(sleep_per_iteration_rand)
-        list_job_dict.append(job_dict)
+        list_jobs_dict.append(job_dict)
+
+    pd_jobs = pd.DataFrame(list_jobs_dict)
+    csv_path = f'./{config_job_site_dict["job_site"]}_{config_job_site_dict["job_title"]}_{datetime_string}.csv'
+    print(f'Saving to {csv_path}')
+    pd_jobs.to_csv(csv_path)
